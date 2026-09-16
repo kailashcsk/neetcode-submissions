a2eb7e9b906d12cc1bb3c6cd37ac59629316d62f
Kailash Shenoy <126532443+kailashcsk@users.noreply.github.com>
2026-09-15 19:55:28 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 3
+print(type(variable))
+variable = 3.4
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Kailash"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
